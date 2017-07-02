04c2daaba48870d0e3a962e52b65349bca0b94a5
GarenLiang <ideabreeze@gmail.com>
2017-07-01 17:36:00 -0700

    Message

--- handlers.go
+++ handlers.go
@@ -27,7 +27,13 @@ type User struct {
 	Id   string `gorethink:"id,omitempty"`
 	Name string `gorethink:"name"`
 }
-
+type ChannelMessage struct {
+	Id        string    `gorethink:"id,omitempty"`
+	ChannelId string    `gorethink:"channelId"`
+	Body      string    `gorethink:"body"`
+	Author    string    `gorethink:"author"`
+	CreatedAt time.Time `gorethink:"createdAt"`
+}
 
 
 func editUser(client *Client, data interface{}) {
@@ -67,6 +73,55 @@ func subscribeUser(client *Client, data interface{}) {
 func unsubscribeUser(client *Client, data interface{}) {
 	client.StopForKey(UserStop)
 }
+
+func addChannelMessage(client *Client, data interface{}) {
+	var channelMessage ChannelMessage
+	err := mapstructure.Decode(data, &channelMessage)
+	if err != nil {
+		client.send <- Message{"error", err.Error()}
+	}
+	go func() {
+		channelMessage.CreatedAt = time.Now()
+		channelMessage.Author = client.userName
+		err := r.Table("message").
+			Insert(channelMessage).
+			Exec(client.session)
+		if err != nil {
+			client.send <- Message{"error", err.Error()}
+		}
+	}()
+}
+
+func subscribeChannelMessage(client *Client, data interface{}) {
+	go func() {
+		eventData := data.(map[string]interface{})
+		val, ok := eventData["channelId"]
+		if !ok {
+			return
+		}
+		channelId, ok := val.(string)
+		if !ok {
+			return
+		}
+		stop := client.NewStopChannel(MessageStop)
+		cursor, err := r.Table("message").
+			OrderBy(r.OrderByOpts{Index: r.Desc("createdAt")}).
+			Filter(r.Row.Field("channelId").Eq(channelId)).
+			Changes(r.ChangesOpts{IncludeInitial: true}).
+			Run(client.session)
+
+		if err != nil {
+			client.send <- Message{"error", err.Error()}
+			return
+		}
+		changeFeedHelper(cursor, "message", client.send, stop)
+	}()
+}
+
+func unsubscribeChannelMessage(client *Client, data interface{}) {
+	client.StopForKey(MessageStop)
+}
+
 
 func addChannel(client *Client, data interface{}) {
 	var channel Channel
